8e8816ca1da10976612b855e8f744e593248cc72
kyle <kyle_derrick@foxmail.com>
2020-09-29 09:30:56 +0800

    init

--- Compilation_principle_exercises/word_analysis/main.c
+++ Compilation_principle_exercises/word_analysis/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Error: not found file parameter!");
+        return 1;
+    }
+    char *fname = argv[1];
+
+    FILE *fp;
+    if (!(fp = fopen(fname, "r"))) {
+        printf("Error: can't open the file!");
+        return 1;
+    }
+
     return 0;
 }
